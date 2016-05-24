42b9988ca110c89a9007c5d443b948d0f0a5f011
Sezer Tunca <sezertunca@gmail.com>
2016-05-24 16:27:35 +0100

    Initial Commit

---
+++ Calculator3/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  Calculator3
+//
+//  Created by Sezer Tunca on 24/05/2016.
+//  Copyright © 2016 Sezer Tunca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(sender: UIButton)
+    {
+        if let digit = sender.currentTitle
+        {
+            let textCurrentlyInDisplay = display.text!
+            
+            if userIsInTheMiddleOfTyping
+            {
+                display.text = textCurrentlyInDisplay + digit
+            }
+            else
+            {
+                display.text = digit
+            }
+            
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    private var displayValue: Double
+    {
+        get
+        {
+            return Double(display.text!)!
+        }
+        set
+        {
+            display.text = String(newValue)
+        }
+    }
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(sender: UIButton)
+    {
+        if userIsInTheMiddleOfTyping
+        {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        if let mathematicalSymbol = sender.currentTitle
+        {
+            brain.performOperation(mathematicalSymbol)
+        }
+        
+        displayValue = brain.result
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
